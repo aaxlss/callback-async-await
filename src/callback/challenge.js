138bd09d42dcc57163f0b53c63a9cdb943f1fb48
aaxlss <axl9305@gmail.com>
2021-08-11 22:17:35 +0100

    Using callbacks

---
+++ src/callback/challenge.js
@@ -0,0 +1,36 @@
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+const API = 'https://rickandmortyapi.com/api/character/'
+
+const fetchData = (url, callback) => {
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.open('GET', url);
+
+    xhttp.onreadystatechange = (event) => {
+        if(xhttp.readyState === 4){
+            if(xhttp.status === 200){
+                callback(null, JSON.parse(xhttp.responseText));
+            } else {
+                const error = new Error(`Error ${url}`);
+                return callback(error, null);
+            }
+        }
+    } 
+    xhttp.send();
+}
+
+
+fetchData(API, (error1, data1) => {
+    if(error1) return console.error(error1);
+
+    fetchData(API + data1.results[0].id,  (error2, data2) => {
+        if(data2) return console.error(error2);
+        fetchData(data2.oring.url, (error3, data3) => {
+            if(error3) return console.error(error3);
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        })
+    });
+});
