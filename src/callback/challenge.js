7f5ab8df6189afe440150ede862c06a8606da850
aaxlss <axl9305@gmail.com>
2021-08-16 21:07:03 +0100

    Configuring promises

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,26 +1,3 @@
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-
-const API = 'https://rickandmortyapi.com/api/character/'
-
-const fetchData = (url, callback) => {
-    let xhttp = new XMLHttpRequest();
-
-    xhttp.open('GET', url);
-
-    xhttp.onreadystatechange = (event) => {
-        if(xhttp.readyState === 4){
-            if(xhttp.status === 200){
-                callback(null, JSON.parse(xhttp.responseText));
-            } else {
-                const error = new Error(`Error ${url}`);
-                return callback(error, null);
-            }
-        }
-    } 
-    xhttp.send();
-}
-
-
 fetchData(API, (error1, data1) => {
     if(error1) return console.error(error1);
 
